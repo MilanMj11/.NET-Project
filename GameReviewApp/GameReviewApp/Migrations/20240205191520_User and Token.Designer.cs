5503b34ce605799f729e6bac3a2d953605430c92
Milan <mujdar.milan@gmail.com>
2024-02-05 21:17:38 +0200

    Refresh Token done

--- GameReviewApp/GameReviewApp/Migrations/20240205191520_User and Token.Designer.cs
+++ GameReviewApp/GameReviewApp/Migrations/20240205191520_User and Token.Designer.cs
@@ -4,6 +4,7 @@ using GameReviewApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameReviewApp.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240205191520_User and Token")]
+    partial class UserandToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
